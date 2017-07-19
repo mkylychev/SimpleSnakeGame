e25e89f6f323d1923d9fc5bec3a2719a0ee17a06
mkylychev <mkylychev@gmail.com>
2017-07-19 16:55:54 +0600

    check for init pyGame

--- snake Game.py
+++ snake Game.py
@@ -3,3 +3,11 @@ import sys
 import random
 import time
 
+check_errors = pygame.init()
+
+if check_errors[1] > 0:
+    print("(!) Had {0} initializing errors".format(check_errors[1]))
+    sys.exit(-1)
+else:
+    print("PyGame successfully initialized")
+
