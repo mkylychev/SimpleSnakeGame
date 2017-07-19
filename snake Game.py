b1b48edac1375a00937fe6be990861a8b08e0288
mkylychev <mkylychev@gmail.com>
2017-07-19 18:34:44 +0600

    init Snake body

--- snake Game.py
+++ snake Game.py
@@ -16,3 +16,25 @@ else:
 play_surface = pygame.display.set_mode((720, 460))
 pygame.display.set_caption('Snake game')
 time.sleep(5)
+
+# Colors
+
+red = pygame.Color(255, 0, 0)
+green = pygame.Color(0, 255, 0)
+black = pygame.Color(0, 0, 0)
+white = pygame.Color(255, 255, 255)
+brown = pygame.Color(165, 25, 25)
+
+# FPS controller
+
+fpsConroller = pygame.time.Clock()
+
+snakePos = [100, 50]
+snakeBody = [[100,50],[90,50],[80,50]]
+
+
+foodPos = [random.randrange(1, 72)*10, random.randrange(1, 46)*10]
+foodSpawn = True
+
+diraction = 'RIGHT'
+changeto = diraction
